08060845bf4e0f293d993f614513aed2daf6874d
JacksonDouglasdeSouza <jacksondouglasdesouza@hotmail.com>
2025-12-13 02:59:27 -0300

    Update

--- index.js
+++ index.js
@@ -19,3 +19,22 @@ document.querySelectorAll(".copy-container").forEach((el) => {
       copiarEndereco(el, el.dataset.endereco);
    });
 });
+
+//---
+
+const scrollToTopBtn = document.getElementById("scrollToTop");
+
+window.addEventListener("scroll", () => {
+   if (window.pageYOffset > 300) {
+      scrollToTopBtn.classList.add("show");
+   } else {
+      scrollToTopBtn.classList.remove("show");
+   }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+   window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+   });
+});
