9f8635d7ddb3a1d781097e5dd82066e44296d6d0
JacksonDouglasdeSouza <jacksondouglasdesouza@hotmail.com>
2026-01-12 06:03:20 -0300

    class

--- aula34-webpack/aula02-webpack/public/assets/js/bundle.js
+++ aula34-webpack/aula02-webpack/public/assets/js/bundle.js
@@ -2,10 +2,22 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-//
-//
-//
-console.log("Olá Webpack!");
+//import { randonNumber } from "./modulo01.js";
+function soma(a, b) {
+  return a + b;
+}
+console.log(soma(2, 3));
+console.log(soma(5, 7));
+console.log(soma(10, 15));
+function multiplica(x, y) {
+  return x * y;
+}
+console.log("---");
+console.log(multiplica(4, 5));
+console.log(multiplica(6, 7));
+console.log(multiplica(8, 9));
+
+//console.log(randonNumber());
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
---
+++ aula34-webpack/aula02-webpack/src/modulo01.js
@@ -0,0 +1,3 @@
+export function randonNumber() {
+   return Math.random();
+}
